bc818346e719612b3318db3ee13e0ae48c2c68ea
yasuomaidana <yasuomaidana@gmail.com>
2025-04-29 15:11:06 -0600

    Using widgets to generate views

--- src/state/app_state.rs
+++ src/state/app_state.rs
@@ -1,7 +1,6 @@
 use crate::message::main_message::MainMessage;
 use crate::widget::main_screen::MainScreen;
-use iced::widget::{button, column, container, row};
-use iced::{Element, Fill, Theme};
+use iced::{Element, Theme};
 
 #[derive(Default)]
 pub struct AppState {
@@ -16,51 +15,34 @@ impl Default for MainScreen {
 
 impl AppState {
     pub(crate) fn title(&self) -> String {
-        format!(
-            "{}",
-            match self.screen {
-                MainScreen::Home => "Home",
-                MainScreen::Register => "Register",
-                MainScreen::MonthlySummary => "Monthly Summary",
-            }
-        )
+        self.screen.title()
     }
 
     pub(crate) fn update(&mut self, message: MainMessage) {
         match message {
             MainMessage::GoToHome => self.screen = MainScreen::Home,
-            MainMessage::GoToRegister => self.screen = MainScreen::Register,
+            MainMessage::GoToRegister => {
+                self.screen = MainScreen::RegisterWidget(Default::default())
+            }
             MainMessage::GoToMonthlySummary => self.screen = MainScreen::MonthlySummary,
+            MainMessage::RegisterMessage(register_message) => match self.screen {
+                MainScreen::RegisterWidget(ref mut prev_state) => {
+                    prev_state.update(register_message);
+                }
+                _ => {
+                    self.screen = MainScreen::RegisterWidget(Default::default());
+                }
+            },
         }
     }
     pub(crate) fn view(&self) -> Element<MainMessage> {
-        match self.screen {
-            MainScreen::Home => {
-                // Create the home screen view
-                let register_button = button("Register").on_press(MainMessage::GoToRegister);
-                let monthly_summary_button =
-                    button("Monthly Summary").on_press(MainMessage::GoToMonthlySummary);
-                let home_row = row![register_button, monthly_summary_button].spacing(20);
-
-                container(home_row)
-                    .center_x(Fill)
-                    .center_y(Fill)
-                    .padding(20)
-                    .into()
-            }
-            _ => {
-                // Create the other screen views
-                let back_button = button("Back").on_press(MainMessage::GoToHome);
-                let content = column![back_button];
-                Element::new(content)
-            }
-        }
+        self.screen.view()
     }
 
     pub(crate) fn theme(&self) -> Theme {
         match self.screen {
             MainScreen::Home => Theme::default(),
-            MainScreen::Register => Theme::Dark,
+            MainScreen::RegisterWidget(_) => Theme::Dark,
             MainScreen::MonthlySummary => Theme::Dark,
         }
     }
