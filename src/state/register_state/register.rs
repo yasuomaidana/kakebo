6feb971ee7fb2c7915f068e68df9cf9f48ad3634
yasuomaidana <yasuomaidana@gmail.com>
2025-04-29 15:10:23 -0600

    Adding mutable methods and update to RegisterState

--- src/state/register_state/register.rs
+++ src/state/register_state/register.rs
@@ -1,4 +1,25 @@
-#[derive(Default, Debug)]
+use crate::message::main_message::RegisterMessage;
+
+#[derive(Default, Debug, Clone)]
 pub struct RegisterState {
     pub count: u32,
+}
+
+impl RegisterState {
+    pub fn increment(&mut self) {
+        self.count += 1;
+    }
+
+    pub fn decrement(&mut self) {
+        if self.count > 0 {
+            self.count -= 1;
+        }
+    }
+
+    pub fn update(&mut self, message: RegisterMessage) {
+        match message {
+            RegisterMessage::Increment => self.increment(),
+            RegisterMessage::Decrement => self.decrement(),
+        }
+    }
 }
